b7d88f6f410dfb0187727532a28d517a20e2fad9
Alex Bui <alexb@rapidflare.ai>
2026-03-03 13:04:00 -0800

    fix dep

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -28,7 +28,7 @@ function Home() {
     refetchInterval: 2000,
   })
 
-  const { mutate, isError } = useMutation({
+  const { mutate, isError: isMutationError } = useMutation({
     mutationFn: async (mode: AcStatus['mode']) => {
       const res = await fetch(`${import.meta.env.VITE_API_URL}/ac-status`, {
         method: 'POST',
@@ -70,8 +70,7 @@ function Home() {
         <svg viewBox="0 0 16 16" width="24" height="24" fill="currentColor"><path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8z"/></svg>
       </a>
       <div className="border border-neutral-800 rounded-2xl p-8 w-full max-w-sm text-center">
-        {isQueryError && <p className="text-red-500 mb-4">Failed to fetch AC status</p>}
-    
+        {(isQueryError || isMutationError) && <p className="text-red-500 mb-4">Failed to fetch...</p>}
         <div className={setting ? 'opacity-50 transition' : 'transition'}>
           {isLoading ? (
             <p className="text-neutral-500">Loading...</p>
